6a4c09153302f039b16c4a18d88a7f3cae6f859b
Janne P Knuutinen <jpknuuti@dhp-10.cs.helsinki.fi>
2013-10-31 13:58:18 +0200

    tetrisjuttu

--- Tetris1/src/tetris1/Main.java
+++ Tetris1/src/tetris1/Main.java
@@ -13,6 +13,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    joo
     public static void main(String[] args) {
         // TODO code application logic here
     }
